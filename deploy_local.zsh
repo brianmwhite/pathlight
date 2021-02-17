c7014a0610612326d59a041d654aa8f115765b4f
Brian White <brian@thirdember.com>
2021-02-16 20:20:18 -0500

    add deploy script

---
+++ deploy_local.zsh
@@ -0,0 +1,17 @@
+#!/bin/zsh
+
+#revert? git reset --hard master@{1}
+
+git fetch
+
+if [ $(git rev-parse HEAD) != $(git rev-parse @{u}) ]; then
+	sudo systemctl stop pathlight
+	sudo rm pathlight.pickle
+
+    git pull
+
+    sudo systemctl start pathlight
+	journalctl -u pathlight -f
+else
+    echo "pathlight already up-to-date"
+fi
