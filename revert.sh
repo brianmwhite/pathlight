0a8e130492281be94a11f2de6b1e803a136fd32e
Brian White <brian@thirdember.com>
2021-03-19 10:24:36 -0400

    updated revert script with similar code to deploy

--- revert.sh
+++ revert.sh
@@ -1,8 +1,32 @@
 #!/usr/bin/bash
-systemctl stop pathlight
-rm pathlight.pickle
 
+SERVICENAME="pathlight"
+DELETEPICKLE=false
+
+echo "reverting code to last version... "
+
+while true; do
+    read -p "Delete saved[pickle] data? [yN] " yn
+    case $yn in
+        [Yy]* ) DELETEPICKLE=true; break;;
+        [Nn]* ) DELETEPICKLE=false; break;;
+        * ) DELETEPICKLE=false; break;;
+    esac
+done
+
+echo "stopping $SERVICENAME service"
+systemctl stop $SERVICENAME
+
+if [ "$DELETEPICKLE" = true ] ; then
+    echo "deleting $SERVICENAME.pickle file"
+    rm $SERVICENAME.pickle
+fi
+
+echo "resetting code to prior version"
 git reset --hard master@{1}
 
-systemctl start pathlight
-journalctl -u pathlight -f
+echo "starting $SERVICENAME service"
+systemctl start $SERVICENAME
+
+echo "viewing logs for $SERVICENAME service"
+journalctl -u $SERVICENAME -f
