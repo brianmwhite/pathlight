c3650f3c0387e326f3b370b8c784dbbeaf161f59
Brian White <brian@thirdember.com>
2021-03-21 15:22:58 -0400

    keep menu open add quit option

--- svcmenu.py
+++ svcmenu.py
@@ -5,22 +5,22 @@ import os
 
 SERVICE_NAME = "pathlight"
 
-def main():
-    print(f"{SERVICE_NAME} service commands...")
-    
-    terminal_menu = TerminalMenu(["stop", "restart", "logs", "start", "status"])
-    menu_entry_index = terminal_menu.show()
-
-    if menu_entry_index == 0:
-        os.system(f"sudo systemctl stop {SERVICE_NAME}")
-    elif menu_entry_index == 1:
-        os.system(f"sudo systemctl restart {SERVICE_NAME}")
-    elif menu_entry_index == 2:
-        os.system(f"journalctl -u {SERVICE_NAME} -f")
-    elif menu_entry_index == 3:
-        os.system(f"sudo systemctl start {SERVICE_NAME}")
-    elif menu_entry_index == 4:
-        os.system(f"systemctl status {SERVICE_NAME}")
-
 if __name__ == "__main__":
-    main()
+    main_menu_exit = False
+
+    while not main_menu_exit:
+        terminal_menu = TerminalMenu(["stop", "restart", "logs", "start", "status", "quit"])
+        menu_entry_index = terminal_menu.show()
+
+        if menu_entry_index == 0:
+            os.system(f"sudo systemctl stop {SERVICE_NAME}")
+        elif menu_entry_index == 1:
+            os.system(f"sudo systemctl restart {SERVICE_NAME}")
+        elif menu_entry_index == 2:
+            os.system(f"journalctl -u {SERVICE_NAME} -f")
+        elif menu_entry_index == 3:
+            os.system(f"sudo systemctl start {SERVICE_NAME}")
+        elif menu_entry_index == 4:
+            os.system(f"systemctl status {SERVICE_NAME}")
+        elif menu_entry_index == 5: 
+            main_menu_exit = True
